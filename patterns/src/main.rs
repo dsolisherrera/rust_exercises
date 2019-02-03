426631e24f523d0f16ad084228e62840994b02d7
Daniel Solis <solisherrerad@gmail.com>
2019-02-03 13:37:25 +0100

    [18.3] Exercises about pattern matching.

---
+++ patterns/src/main.rs
@@ -0,0 +1,22 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    let sum_of_squares: i32 = points.iter().map(|Point { x, y }| x * x + y * y).sum();
+
+    println!("{}", sum_of_squares);
+
+
+    
+    let ((_, _), Point {x: _, y}) = ((3, 10), Point { x: 3, y: -10 });
+    println!("{}", y);
+
+}
